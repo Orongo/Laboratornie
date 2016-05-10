5e4e4e35c1b91a4f6e4fdb8b5463ca9de0ddd555
mdprkr <mdprker@gmail.com>
2016-05-11 00:33:51 +0300

    5-PopularWord_first_commit

---
+++ lab-2/5-PopularWord/main.py
@@ -0,0 +1,10 @@
+def get_words(lines):
+	words = {}
+	lines = "".join([i for i in lines if not i in '!,.?;:"!@#$%^&*()_-+=<>/}{[]|'])
+	for word in lines.replace('\n',' ').split(' '):
+		words[word] = words[word]+1 if word in words else 1
+	return [[i,words[i]] for i in sorted(words, key=words.get, reverse = True)]
+def get_popular(words):
+	return "---" if (len(words)>1) and (words[0][1] == words[1][1]) else words[0][0]
+with open('input.txt','r') as f:
+	print(get_popular(get_words(f.read())))
